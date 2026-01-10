b0df7d4701352aeca9bb3b949aedaf56fd210f3b
Andrei Balan <andrei.balan98@gmail.com>
2026-01-11 01:03:30 +0200

    quick fix - action penalty

--- env_wrapper.py
+++ env_wrapper.py
@@ -11,17 +11,20 @@ class FlappyBirdWrapper(gym.Wrapper):
     - Resize la 84x84
     - Stack de 4 frame-uri
     - Normalizare la [0, 1]
-    - Frame skip pentru acțiuni mai consistente
+    - Action penalty pentru jump spam
     """
     
-    def __init__(self, env, img_size=84, stack_frames=4, frame_skip=4):
+    def __init__(self, env, img_size=84, stack_frames=4):
         super().__init__(env)
         self.img_size = img_size
         self.stack_frames = stack_frames
-        self.frame_skip = frame_skip  # Nou: frame skip
         
         # Deque pentru stocarea frame-urilor
         self.frames = deque(maxlen=stack_frames)
+        
+        # Tracking pentru action penalty
+        self.last_action = 0
+        self.consecutive_jumps = 0
         
         # Actualizează observation space
         self.observation_space = gym.spaces.Box(
@@ -47,6 +50,10 @@ class FlappyBirdWrapper(gym.Wrapper):
     def reset(self, **kwargs):
         obs, info = self.env.reset(**kwargs)
         
+        # Reset action tracking
+        self.last_action = 0
+        self.consecutive_jumps = 0
+        
         # Preprocesează frame-ul inițial
         frame = self._preprocess_frame(obs)
         
@@ -64,18 +71,31 @@ class FlappyBirdWrapper(gym.Wrapper):
         self.frames.append(frame)
         
         # Reward shaping pentru învățare mai bună
-        # Mediul oferă: +0.1 per frame, +1.0 per tub
         shaped_reward = reward
         
-        # La moarte: penalizare mică (nu drastică)
+        # Penalizare la moarte
         if terminated:
             shaped_reward = -1.0
-        # Per frame supraviețuit: reward pozitiv
-        elif reward <= 0.2:  # Doar supraviețuire
+        # Per frame supraviețuit
+        elif reward <= 0.2:
             shaped_reward = 0.1
-        # Tub trecut: bonus mare!
-        else:  # reward >= 1.0
+        # Tub trecut - bonus mare!
+        else:
             shaped_reward = 1.0
+        
+        # ACTION PENALTY: Penalizează jump-uri consecutive excesive
+        if action == 1:  # Jump
+            if self.last_action == 1:
+                self.consecutive_jumps += 1
+                # Penalizare crescătoare pentru spam
+                shaped_reward -= 0.05 * self.consecutive_jumps
+            else:
+                self.consecutive_jumps = 1
+        else:  # Do nothing
+            self.consecutive_jumps = 0
+        
+        # Update last action
+        self.last_action = action
         
         return self._get_stacked_frames(), shaped_reward, terminated, truncated, info
     
